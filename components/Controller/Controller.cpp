118112fc708fc8f3d27790d81e99f2df72f04d07
lukas.petrikas <lukas.petrikas@teltonika.lt>
2021-09-02 11:25:58 +0300

    first commit

---
+++ components/Controller/Controller.cpp
@@ -0,0 +1,54 @@
+
+#include "Controller.h"
+
+Controller::Controller(){
+    printf("Thermostat object created \n");
+}
+
+void Controller::begin(){
+    this->test1 = 0;
+    this->old_state = INITIAL;
+    this->main_task_handle = NULL;
+    printf("Start the task1 \n");
+    this->State_change_handle(MODE2);
+}
+
+void Controller::State_change_handle(e_thermostat_state state)
+{
+    this-> new_state = state;
+    if(this-> old_state != this-> new_state ){ // if the new state is different than current state, delete the task
+        printf("different state, delete the previous task \n");
+        if (this ->main_task_handle != NULL){
+            vTaskDelete(this ->main_task_handle);
+        }
+    }
+    else
+    {
+        printf("same task \n"); // the same task is set, dont do anything
+        return;
+    }
+        
+    switch(this->new_state) 
+    {   
+        case MODE1:
+            printf("NEW STATE = MODE1 \n");
+            this-> old_state = MODE1;
+            xTaskCreate(task1,"MODE1",10000,this,1,&this->main_task_handle); // receiving commands from main uart
+            break;
+
+        case MODE2:
+            printf("NEW STATE = MODE2\n");
+            this-> old_state = MODE2;
+            xTaskCreate(task2,"MODE2",10000,this,1,&this->main_task_handle); // receiving commands from main uart
+            break;
+
+        default:
+            printf("state not recognised \n");
+    }
+}
+
+
+
+
+
+
---
+++ main/main.cpp
@@ -0,0 +1,18 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include "main.h"
+
+Controller controller;
+
+void app_main(void)
+{
+    //xTaskCreate(SystemUART_Task,"MainTask",10000,NULL,1,NULL); // receiving commands from main uart
+    controller.begin();
+}
